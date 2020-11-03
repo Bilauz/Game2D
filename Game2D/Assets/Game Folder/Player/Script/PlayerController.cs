f3b9ddf099a870fa2b919c95f71bd283ce7dfd6f
Vinícius Dias Aresi <contatovdaresi@gmail.com>
2020-11-03 00:59:13 -0300

    Atualização, Movimentação do personagem

---
+++ Game2D/Assets/Game Folder/Player/Script/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Rigidbody2D rig;
+
+    Vector2 vel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        vel = new Vector2(Input.GetAxisRaw("Horizontal"), rig.velocity.y);
+
+
+    }
+
+    void FixedUpdate()
+    {
+        rig.velocity = vel;
+    }
+}
